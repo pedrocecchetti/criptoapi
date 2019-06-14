e5caee4bed6d7641710699126e9d7178eee66c89
cecchetti <pedro.cecchetti@gmail.com>
2019-06-14 17:50:13 -0300

    feat:add updateDB route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get '/criptoinfo' , to: 'cripto_info#index'
+      get '/updatedatabase', to: 'cripto_info#updatecriptoinfo'
     end
   end
 end
