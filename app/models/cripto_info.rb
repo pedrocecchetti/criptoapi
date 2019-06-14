14dba51a2dfdba6f22f28d1924b82ebb6c0081fc
cecchetti <pedro.cecchetti@gmail.com>
2019-06-14 12:39:23 -0300

    feat: add validation to criptoinfo

--- app/models/cripto_info.rb
+++ app/models/cripto_info.rb
@@ -1,2 +1,6 @@
 class CriptoInfo < ApplicationRecord
+    validates :name, presence: true
+    validates :symbol, presence: true
+    validates :last_updated, presence: true
+    validates :price, presence: true
 end
