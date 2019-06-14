c9f05cb7d77131b6b603f16a2e4fa45e74cb5a06
cecchetti <pedro.cecchetti@gmail.com>
2019-06-14 12:21:00 -0300

    feat: add route to retrieve criptoinfo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see 
+  namespace 'api' do
+    namespace 'v1' do
+      get '/criptoinfo' , to: 'cripto_info#index'
+    end
+  end
 end
