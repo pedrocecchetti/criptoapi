e78ca39b72a7a85238210933d3d4a6b7126288a0
cecchetti <pedro.cecchetti@gmail.com>
2019-06-15 11:05:10 -0300

    feat: add updateDB route

--- app/controllers/api/v1/cripto_info_controller.rb
+++ app/controllers/api/v1/cripto_info_controller.rb
@@ -1,9 +1,54 @@
+require 'net/http'
+
 module Api
     module V1
         class CriptoInfoController<ApplicationController
             def index
-                critpoinfo = CriptoInfo.order('id')
-                render json: {status:'Mandou bem', message:'Successful request', data:critpoinfo}, status: :ok
+                criptoinfo = CriptoInfo.order('id')
+                render json: {status:'200', message:'Successful request', data:criptoinfo}, status: :ok
+            end
+
+            def updatecriptoinfo
+                # Defining API KEY to make the get request
+                api_key = '618a961a-29df-4ea4-96e1-f0c53b35d2fd'
+                
+                # Building API Key
+                uri = URI('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=30&convert=USD&CMC_PRO_API_KEY=' + api_key)
+                
+                # Making GET request
+                res = Net::HTTP::get(uri)
+
+
+                if res
+                    res_obj = JSON.parse(res)['data']
+                    for curr in res_obj
+                        api_id = curr['id'].to_i
+                        
+                        db_instance = CriptoInfo.find_by(api_id: api_id)
+                        
+                        if db_instance
+                            db_instance.price = curr['quote']['USD']['price']
+                            db_instance.last_updated = curr['last_updated']
+                            db_instance.save
+                            transaction = "UPDATED RECORD"
+                        else
+                            criptoinfo = CriptoInfo.new
+                            criptoinfo.name = curr['name']
+                            criptoinfo.symbol = curr['symbol']
+                            criptoinfo.last_updated = curr['last_updated']
+                            criptoinfo.price = curr['quote']['USD']['price']
+                            criptoinfo.api_id = curr['id']
+                            criptoinfo.save
+                            transaction = "CREATED NEW RECORD"
+                        end
+                    end
+                    render json: {messsage:"Database Sucessfully updated", status:transaction}, status: :ok
+                else
+                    render json: {messsage:"API not reachable"}, status: :ok
+            end
+
+
+                
             end
         end
     end
